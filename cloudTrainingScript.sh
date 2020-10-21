f09820808b6425c713eb45336723bc7fa100554d
Justin Guese <guese.justin@gmail.com>
2020-10-21 18:26:21 +0200

    req and cloud

---
+++ cloudTrainingScript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo yum install git -y
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /home/ec2-user/miniconda.sh
+cd /home/ec2-user
+bash ./miniconda.sh -b -p /home/ec2-user/miniconda
+git clone https://github.com/JustinGuese/trading-bot
+cd trading-bot/
+/home/ec2-user/miniconda/bin/conda init bash
+source /home/ec2-user/.bashrc
+conda create --name trading -y
+source activate trading
+conda install pip -y 
+pip install -r requirements_trainingOnly.txt
+# reqs
+
+
+screen -S trainer
+python train.py data/BTCUSD\=X_1d_all.csv data/BTCUSD\=X_1d_test.csv --model-name btcusd1d
+git add -A
+git commit -m "training done"
+git push origin master
+# this will need sudo
+shutdown -h now
