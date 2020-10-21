61aa995f9df764cef3d69097a7975d5af1ea5918
Justin Guese <guese.justin@gmail.com>
2020-10-21 18:31:43 +0200

    cloud script udpate

--- cloudTrainingScript.sh
+++ cloudTrainingScript.sh
@@ -3,14 +3,17 @@ sudo yum install git -y
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /home/ec2-user/miniconda.sh
 cd /home/ec2-user
 bash ./miniconda.sh -b -p /home/ec2-user/miniconda
-git clone https://github.com/JustinGuese/trading-bot
-cd trading-bot/
+git clone https://github.com/JustinGuese/reinforcement-learning-trading-bot
+cd reinforcement-learning-trading-bot
 /home/ec2-user/miniconda/bin/conda init bash
 source /home/ec2-user/.bashrc
 conda create --name trading -y
 source activate trading
 conda install pip -y 
-pip install -r requirements_trainingOnly.txt
+# sometimes theres a memory error, use this instead
+pip install --no-cache-dir tensorflow
+pip install -r requirements.txt
+sudo chown -R ec2-user:ec2-user .
 # reqs
 
 
