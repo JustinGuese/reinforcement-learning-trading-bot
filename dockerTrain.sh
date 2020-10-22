5af98869221d03ebc0e314dc6177a5598a278897
Justin Guese <guese.justin@gmail.com>
2020-10-22 21:20:44 +0200

    looking goood

--- dockerTrain.sh
+++ dockerTrain.sh
@@ -2,4 +2,4 @@
 # add what you want to train simulataneaously
 exec ls data/ & 
 exec python train.py "data/BTCUSD=X_1d_train.csv.gz" "data/BTCUSD=X_1d_test.csv.gz" --model-name btcusd1d --episode-count 200 &
-exec python train.py "data/AAPL_1d_train.csv.gz" "AAPL_1d_test.csv.gz" --model-name aapl1d --episode-count 200
+exec python train.py "data/AAPL_1d_train.csv.gz" "data/AAPL_1d_test.csv.gz" --model-name aapl1d --episode-count 200
